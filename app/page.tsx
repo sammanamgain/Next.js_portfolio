2239aa246b3f7395eefd3fa1de58171edae293e2
sammanamgain <amgain02@gmail.com>
2024-02-25 21:22:40 +0545

    about page setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Intro from "@/components/intro";
-
+import SectionDivider from "@/components/sectiondivider";
+import About from "@/components/about";
 export default function Home() {
   return (
     <main className='flex flex-col items-center px-4'>
       <Intro></Intro>
+      <SectionDivider />
+      <About></About>
     </main>
   );
 }
