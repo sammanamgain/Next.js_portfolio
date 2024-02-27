4dab7a2cc26b604530bd6d1b81886c414abeedd0
sammanamgain <amgain02@gmail.com>
2024-02-27 21:19:58 +0545

    skill section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import Intro from "@/components/intro";
 import SectionDivider from "@/components/sectiondivider";
 import About from "@/components/about";
-import Projects from "@/components/projects"
+import Projects from "@/components/projects";
+import Skills from "@/components/skills";
 export default function Home() {
   return (
     <main className='flex flex-col items-center px-4'>
@@ -9,6 +10,7 @@ export default function Home() {
       <SectionDivider />
       <About></About>
       <Projects></Projects>
+      <Skills></Skills>
     </main>
   );
 }
